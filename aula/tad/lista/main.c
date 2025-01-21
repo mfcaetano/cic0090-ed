3f15487106af1f73529ebc21deb9724effd3a084
Marcos F. Caetano <mfcaetano@unb.br>
2025-01-21 10:07:37 -0300

    - movendo implementação de lista.

--- aula/tad/lista/main.c
+++ aula/tad/lista/main.c
@@ -1,40 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "lista.h"
 
-struct lista_compras{
-    char nome_produto[20];
-    float valor;
-};
+int main(){
+    Lista* lista = cria_lista();
+    
+   
 
-void imprime_lista(struct lista_compras* lista, int n){
-    for(int i = 0; i < n; i++){
-        printf("%s %.2f\n", lista[i].nome_produto, lista[i].valor);
-    }
-}
+    add(lista, 4);
+    add(lista, 5);
+    add(lista, 14);
+    add(lista, 114);
+    append(lista, 40);
+    
+    insert(lista, 2, 200);
 
+    imprime_lista(lista);
 
-int main() {
-    struct lista_compras minha_lista[5];
-
-    for(int i = 0; i < 3; i++){
-        printf("Informe o nome do produto: \n");
-        scanf("%[^\n]s", minha_lista[i].nome_produto);
-        getchar();
-
-        printf("Informe o valor:\n");
-        scanf("%f", &minha_lista[i].valor);
-        getchar();
-    }
-
-    imprime_lista(minha_lista, 3);
+ 
 
 
 
 
-    printf("Hello world!\n");
-    return 0;
-
-
-
-
+    printf("hello \n");
 }
