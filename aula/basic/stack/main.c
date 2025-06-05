819b761dc85d11cf1d55e0eace9ff7b83d35a322
Marcos Caetano <mfcaetano@unb.br>
2025-06-05 14:27:26 -0300

    -código de pilha desenvolvido na aula do dia 05/06/25.

--- aula/basic/stack/main.c
+++ aula/basic/stack/main.c
@@ -9,26 +9,28 @@ int main(){
   char nome[200];
   int idade;
   Student *pt_student;
+  Stack* stack = create_stack();
   
-  printf("Informe seu nome:\n");
-  scanf("%[^\n]s", nome);
-  printf("Informe sua idade: \n");
-  scanf("%d", &idade);
+  for(int i = 0; i < 3; i++){
+  
+    printf("Nome aluno %d:\n", i+1);
+    scanf("%[^\n]s", nome);
+    printf("Idade aluno %d:\n", i+1);
+    scanf("%d", &idade);
+    getchar();
+    
+    push(stack, create_student(nome, idade));
+    
+  }//end for i
   
   //print_student(pt_student);
   
-  
-  //free_student(pt_student);
-  
-  Stack* stack = create_stack();
-  pt_student = create_student(nome, idade);
-
-  
-  push(stack, pt_student);
-  
-  
-  //pt_aux = pop(stack);
-  
+  for(int i = 0; i < 3; i++){
+    pt_student = pop(stack);
+    print_student(pt_student);
+   
+    free_student(pt_student);
+  }
   
   free_stack(stack);
 }//end main()
---
+++ coderun/stack/separador/main_pilha_separador_coderun.c
@@ -0,0 +1,145 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+  char *item;
+  struct Node* next;
+} Node;
+
+typedef struct{
+  Node* top;
+  int size;
+} Stack;
+
+Stack* create_stack();
+void free_stack(Stack* stack);
+
+Node* create_node(const char* item);
+void free_node(Node* node);
+
+//empilhar um aluno na pilha
+void push(Stack* stack, const char* item);
+
+//desempilhar um aluno
+char* pop(Stack* stack);
+
+//imprime pilha
+void print_stack(Stack* stack);
+
+
+
+Stack* create_stack(){
+  Stack* stack = (Stack *) malloc(sizeof(Stack));
+  stack->top = NULL;
+  stack->size = 0;
+  
+  return stack;
+}//end create_stack();
+
+
+void free_stack(Stack* stack){
+  Node* pt_aux = stack->top;
+
+  while(pt_aux){
+    stack->top = stack->top->next;
+    free_node(pt_aux);
+    
+    pt_aux = stack->top;
+  }//end while
+
+  free(stack);
+}//end free_stack()
+
+
+Node* create_node(const char* item){
+  Node* node = (Node *) malloc(sizeof(Node));
+  
+  node->item = (char *) malloc(strlen(item)+1);
+  strcpy(node->item, item);
+  
+  node->next = NULL;
+  
+  return node;
+}//end create_node()
+
+
+void free_node(Node* node){
+    free(node->item);
+    free(node);   
+}//end free_node()
+
+
+//empilhar um aluno na pilha
+void push(Stack* stack, const char* item){
+  Node* node = create_node(item);
+  
+  node->next = stack->top;
+  stack->top = node;
+  stack->size++;
+}//end push()
+
+
+//desempilhar um aluno
+char* pop(Stack* stack){
+  if(stack->size == 0)
+    return NULL;
+  
+  char* item = stack->top->item;
+  Node* pt_aux = stack->top;
+  stack->top = stack->top->next;
+  stack->size--;
+  
+  free(pt_aux);
+  return item;
+}//end pop
+
+
+
+void print_stack(Stack* stack){
+  if(stack->size == 0)
+    return;
+    
+  Node* it = stack->top;
+  
+  while(it){
+    printf("%s\n", it->item);
+    
+    it = it->next;
+  }//end while
+}//end print_stack()
+
+
+
+int main(){
+    char uinput[200];
+    Stack* palavras = create_stack();
+    Stack* numeros = create_stack();
+    
+    int flag = 0;
+    while(!flag){
+        scanf("%s", uinput);
+        
+        if(strcmp(uinput, "end")){ 
+            //verifica se é número
+            if(uinput[0] <= 57 && uinput[0] >= 48){
+              push(numeros, uinput);
+            }else{
+              push(palavras, uinput);
+            }
+            
+        }else{
+            flag = 1;
+        }
+    }//end while
+    
+    printf("Palavras:\n");
+    print_stack(palavras);
+
+    printf("Numeros:\n");
+    print_stack(numeros);
+    
+    free_stack(palavras);
+    free_stack(numeros);
+}//end main()
